6f78943734a2353395e5ebda63d03705f55fd0a3
Alex Huang <alex_huang@wistron.com>
2018-06-08 20:41:15 +0800

    temp

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { IonicPage, NavController } from 'ionic-angular';
 
-import { Tab1Root, Tab2Root, Tab3Root } from '../';
+//import { Tab1Root, Tab2Root } from '../';
+import { Tab3Root } from '../';
 import { SubordinatesPage } from '../subordinates/subordinates';
 import { MyTimesheetPage } from '../my-timesheet/my-timesheet';
 
@@ -17,22 +18,25 @@ export class TabsPage {
   tabTitleMyTSS = " ";
   tabTitleSubordinates = " ";
 
-  tab1Root: any = Tab1Root;
-  tab2Root: any = Tab2Root;
+  //tab1Root: any = Tab1Root;
+  //tab2Root: any = Tab2Root;
   tab3Root: any = Tab3Root;
 
-  tab1Title = " ";
-  tab2Title = " ";
+  //tab1Title = " ";
+  //tab2Title = " ";
   tab3Title = " ";
 
   constructor(public navCtrl: NavController, public translateService: TranslateService) {
+    /*
     translateService.get(['TAB1_TITLE', 'TAB2_TITLE', 'TAB3_TITLE']).subscribe(values => {
       this.tab1Title = values['TAB1_TITLE'];
       this.tab2Title = values['TAB2_TITLE'];
       this.tab3Title = values['TAB3_TITLE'];
     });
-    translateService.get(['MY_TIMESHEET', 'TAB3_TITLE']).subscribe(values => {
-      this.tabTitleMyTSS = values['MY_TIMESHEET'];
+    */
+    translateService.get(['TAB_TITLE_MY_TIMESHEET', 'TAB_TITLE_REVIEW_TIMESHEET', 'TAB3_TITLE']).subscribe(values => {
+      this.tabTitleMyTSS = values['TAB_TITLE_MY_TIMESHEET'];
+      this.tabTitleSubordinates = values['TAB_TITLE_REVIEW_TIMESHEET'];
       this.tab3Title = values['TAB3_TITLE'];
     });
   }
